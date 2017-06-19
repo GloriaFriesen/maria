ba1af717bef95bc417b138e9370c2ac8b05c8a4d
Gloria Friesen <gloriafriesen@gmail.com>
2017-06-19 13:39:52 -0700

    add tile map

---
+++ android/assets/untitled.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tiles_spritesheet" tilewidth="16" tileheight="16" spacing="2" margin="1" tilecount="2600" columns="50">
+ <image source="tiles_spritesheet.png" width="914" height="936"/>
+</tileset>
