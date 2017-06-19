b1566106335bb99db1fc5b012114f860fc768c2c
Gloria Friesen <gloriafriesen@gmail.com>
2017-06-19 09:01:27 -0700

    initial libgdx setup

---
+++ desktop/src/com/gloria/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gloria.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gloria.game.Maria;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Maria(), config);
+	}
+}
