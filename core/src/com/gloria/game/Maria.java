aefb149b41c5e4c0081002554da1acb58212c1bf
Gloria Friesen <gloriafriesen@gmail.com>
2017-06-19 13:59:30 -0700

    render tilemap

--- core/src/com/gloria/game/Maria.java
+++ core/src/com/gloria/game/Maria.java
@@ -9,8 +9,8 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.gloria.game.screens.PlayScreen;
 
 public class Maria extends Game {
-	public static final int V_WIDTH = 400;
-	public static final int V_HEIGHT = 208;
+	public static final int V_WIDTH = 600;
+	public static final int V_HEIGHT = 800;
 
 	public SpriteBatch batch;
 
--- core/src/com/gloria/game/screens/PlayScreen.java
+++ core/src/com/gloria/game/screens/PlayScreen.java
@@ -5,6 +5,9 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
@@ -17,12 +20,19 @@ public class PlayScreen implements Screen {
     private OrthographicCamera gameCam;
     private Viewport gamePort;
     private Hud hud;
+    private TmxMapLoader maploader;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
 
     public PlayScreen(Maria game) {
         this.game = game;
         gameCam = new OrthographicCamera();
         gamePort = new FitViewport(Maria.V_WIDTH, Maria.V_HEIGHT, gameCam);
         hud = new Hud(game.batch);
+        maploader = new TmxMapLoader();
+        map = maploader.load("maria.tmx");
+        renderer = new OrthogonalTiledMapRenderer(map);
+        gameCam.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() / 2, 0);
     }
 
     @Override
@@ -30,10 +40,26 @@ public class PlayScreen implements Screen {
 
     }
 
+    public void handleInput(float delta) {
+        if (Gdx.input.isTouched())
+            gameCam.position.y += 100 * delta;
+    }
+
+    public void update(float delta) {
+        handleInput(delta);
+
+        gameCam.update();
+        renderer.setView(gameCam);
+    }
+
     @Override
     public void render(float delta) {
+        update(delta);
+
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        renderer.render();
 
         game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
         hud.stage.draw();
