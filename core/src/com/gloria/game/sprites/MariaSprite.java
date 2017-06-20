c62ce108db8b13187d01a2891d004323a0114b4a
Gloria Friesen <gloriafriesen@gmail.com>
2017-06-20 11:44:30 -0700

    start run and jump animation

--- core/src/com/gloria/game/sprites/MariaSprite.java
+++ core/src/com/gloria/game/sprites/MariaSprite.java
@@ -12,6 +12,7 @@ import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.Array;
 import com.gloria.game.Maria;
 import com.gloria.game.screens.PlayScreen;
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
 
 public class MariaSprite extends Sprite {
     public enum State { FALLING, JUMPING, STANDING, RUNNING };
@@ -20,8 +21,8 @@ public class MariaSprite extends Sprite {
     public World world;
     public Body b2body;
     private TextureRegion mariaStand;
-    private Animation<TextureRegion> marioRun;
-    private Animation<TextureRegion> marioJump;
+    private Animation<TextureRegion> mariaRun;
+    private Animation<TextureRegion> mariaJump;
     private boolean runningRight;
     private float stateTimer;
 
@@ -33,18 +34,62 @@ public class MariaSprite extends Sprite {
         stateTimer = 0;
         runningRight = true;
 
-//        Array<TextureRegion> frames = new Array<TextureRegion>();
-//        for (int i = 1; i < 4; i++)
-//            frames.add(new TextureRegion(getTexture(), i * 50, 0, 16, 16))
+        Array<TextureRegion> frames = new Array<TextureRegion>();
+        for (int i = 8; i < 6; i++)
+            frames.add(new TextureRegion(getTexture(), i * 32, 0, 32, 32));
+        mariaRun = new Animation<TextureRegion>(0.1f, frames);
+        frames.clear();
+
+        for (int i = 1; i < 7; i++)
+            frames.add(new TextureRegion(getTexture(), i * 32, 0, 32, 32));
+        mariaJump = new Animation<TextureRegion>(0.1f, frames);
+        frames.clear();
+
+        mariaStand = new TextureRegion(getTexture(), 0, 0, 32, 32);
 
         defineMaria();
-        mariaStand = new TextureRegion(getTexture(), 0, 0, 32, 32);
         setBounds(0, 0, 32 / Maria.PPM, 32 / Maria.PPM);
         setRegion(mariaStand);
     }
 
     public void update(float delta) {
         setPosition(b2body.getPosition().x - getWidth() / 2, b2body.getPosition().y - getHeight() / 2);
+        setRegion(getFrame(delta));
+    }
+
+    public TextureRegion getFrame(float delta) {
+        currentState = getState();
+
+        TextureRegion region;
+        switch (currentState) {
+            case JUMPING:
+                region = mariaJump.getKeyFrame(stateTimer);
+                break;
+            case RUNNING:
+                region = mariaRun.getKeyFrame(stateTimer, true);
+                break;
+            case FALLING:
+            case STANDING:
+                default:
+                    region = mariaStand;
+                    break;
+        }
+
+        if ((b2body.getLinearVelocity().x < 0 || !runningRight) && !region.isFlipX()) {
+            region.flip(true, false);
+            runningRight = false;
+        }
+    }
+
+    public State getState() {
+        if (b2body.getLinearVelocity().y > 0)
+            return State.JUMPING;
+        else if (b2body.getLinearVelocity().y < 0)
+            return State.FALLING;
+        else if (b2body.getLinearVelocity().x != 0)
+            return State.RUNNING;
+        else
+            return State.STANDING;
     }
 
     public void defineMaria(){
